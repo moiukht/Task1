a1615c7a641a38eb6f210e98fd96926b441a65f2
Tahsina Moiukh <142219954+moiukht@users.noreply.github.com>
2025-04-16 01:01:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from datetime import datetime
 from nlp import handle_user_message
 from db.connection import database_connect
 import nltk
-nltk.download('punkt') 
+nltk.download('punkt', download_dir='C:/Users/User/nltk_data')
+nltk.download('punkt_tab' , download_dir='C:/Users/User/nltk_data')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
 app = FastAPI()
 @app.get('/')
 
